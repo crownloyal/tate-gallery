c19ff3ff618dc788db2916b30da56a57643ae2d7
Dominic M Brause <dom.brause@gmail.com>
2017-11-26 16:08:11 +0000

    + sails bootstrapping

---
+++ server/config/bootstrap.js
@@ -0,0 +1,20 @@
+/**
+ * Bootstrap
+ * (sails.config.bootstrap)
+ *
+ * An asynchronous bootstrap function that runs just before your Sails app gets lifted.
+ * > Need more flexibility?  You can also do this by creating a hook.
+ *
+ * For more information on bootstrapping your app, check out:
+ * https://sailsjs.com/config/bootstrap
+ */
+
+module.exports.bootstrap = function(done) {
+
+  // By convention, this is a good place to set up fake data during development.
+
+  // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
+  // (otherwise your server will never lift, since it's waiting on the bootstrap)
+  return done();
+
+};
